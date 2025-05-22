a51c62b7c054bf2d1d09429883da611d197ff516
Maksim <mowgzem@gmail.com>
2025-05-22 22:14:27 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import axios from 'axios';
+    import { useUserStore } from '~/store/user';
+
+    axios.defaults.withCredentials = true;
+    axios.defaults.withXSRFToken = true;
+
+    const runtimeConfig = useRuntimeConfig();
+    axios.defaults.baseURL = runtimeConfig.public.api.baseUrl;
+
+    await axios.get('/sanctum/csrf-cookie');
+    
+    let auth = useUserStore();
+    auth.getUser();
+</script>
+
+<template>
+    <NuxtRouteAnnouncer />
+    <MainNav></MainNav>
+    <NuxtPage></NuxtPage>
+</template>
